db963249628b84d1b46535a9df96a846352b4b96
Raihan Daffa Izzuddin <raihandaffaizzuddin77@gmail.com>
2026-03-09 23:19:10 +0700

    commit jobsheet 5 filament

--- PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -13,7 +13,9 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\IconColumn;
-
+use Filament\Schemas\Components\Section;
+use Filament\Support\Assets\HeroIcon;
+use Filament\Schemas\Components\Group;
 
 
 class PostForm
@@ -23,6 +25,12 @@ class PostForm
         return $schema
             ->components([
                 //
+                Section::make('Post Details')
+                    ->Description ('Fill in the details of the post.')
+                    // ->icon(HeroIcon::RocketLaunch)
+                    ->icon('heroicon-o-document-text')
+                    ->schema([
+                Group::make([
                 TextInput::make('title')
                     ->required()
                     ->minLength(5),
@@ -34,16 +42,33 @@ class PostForm
                     ->preload()
                     ->searchable(),
                 ColorPicker::make('color'),
-                // MarkdownEditor::make('content'),
-                RichEditor::make('content'),
+                ])->columns(2),
+                MarkdownEditor::make('content'),
+                ])->columnSpan(2),
+
+                Group::make([
+
+                Group::make([
+                Section::make('Image Upload')
+                    ->icon('heroicon-o-photo')
+                    ->schema([
+                // RichEditor::make('content'),
                 FileUpload::make('image')
                     ->disk('public')
                     ->directory('posts'),
+                ]),
+
+                Section::make('Meta Information')
+                    ->icon('heroicon-o-tag')
+                    ->schema([
                 TagsInput::make('tags'),
                 Checkbox::make('published'),
                 DatePicker::make('published_at'),
                 IconColumn::make('published')
                     ->boolean(),
-            ]);
+                ]),
+            ])
+        ])->columnSpan(1)
+    ])->columns(3);
     }
 }
